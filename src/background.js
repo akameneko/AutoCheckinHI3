10da68ec36344da0419ec1ee907d39c284cc7db1
akameneko <optimus.prime.vtd95@gmail.com>
2022-08-13 01:32:16 +0700

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,15 +1,17 @@
-﻿'use strict';
+'use strict';
 chrome.runtime.onStartup.addListener(function(){
     setTimeout(onload,10000);
 });
-function onload(){
-    let request = new XMLHttpRequest();
-    request.open("POST","https://api-os-takumi.mihoyo.com/event/mani/sign?lang=vi-vn", true);
-    request.setRequestHeader("Content-Type","application/json;charset=UTF-8");
-    request.setRequestHeader("Accept","application/json, text/plain, */*");
-    request.setRequestHeader("Accept-Language", "vi,en;q=0.9,ja;q=0.8,ht;q=0.7");
-    request.onload = function(){
-        console.log("Status: " + this.status + ", Response: " + this.responseText);
+async function onload() {
+    console.log("start");
+    try {
+        var res = await fetch(
+            "https://sg-public-api.hoyolab.com/event/mani/sign?lang=vi-vn",
+            { method: "POST", body: JSON.stringify({ act_id: "e202110291205111" }) });
+        console.log(await res.text());
     }
-    request.send("{\"act_id\":\"e202110291205111\"}");
+    catch (e) {
+        console.log(e);
+        setTimeout(onload, 5000);
+    }
 }
